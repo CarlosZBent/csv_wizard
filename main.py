0e5c7f44dd30fe608aed6d7174650a2eeeca53ca
carloszbent <bentsoft365@gmail.com>
2023-02-07 21:26:38 -0500

    UNSTABLE added get_encoding method

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from typing import Type, IO
 from pathlib import Path
 from csv import reader, writer, Sniffer, Dialect
+from chardet import detect
 
 
 class FileReader:
     def __init__(self, source:str) -> None:
         # concat the .csv extension so it is not necessary when instatiating
         self.source = f'{source}.csv'
+
+    def __get_encoding(self):
+        """
+        Detect the encoding of the file
+        """
+        with open(self.source, 'rb') as file:
+            line = file.readline()
+            encoding = detect(line)['encoding']
+            print(encoding)
+            return encoding
 
     def __open(self, mode:str) -> IO:
         """
@@ -40,13 +51,13 @@ class FileReader:
         Private method to get the amount of rows that 
         the file contains, excluding the headers row.
         """
-        file = self.__open('r')
-        parser = reader(file, delimiter=self.get_dialect().delimiter)
-        row_count = -1
-        for line in parser:
-            row_count += 1
-        file.close()
-        return row_count
+        encoding = self.__get_encoding()
+        with open(self.source, 'r', encoding=encoding) as file:
+            parser = reader(file, delimiter=self.get_dialect().delimiter)
+            row_count = -1
+            for line in parser:
+                row_count += 1
+            return row_count
 
     def get_headers(self) -> list[str]:
         """
@@ -76,29 +87,30 @@ class FileReader:
         Divide the file's rows exactly in half if possible.
         If the total number of rows is odd the first half will contain one extra row.
         """
-        file = self.__open('r')
-        parser = reader(file, delimiter=self.get_dialect().delimiter)
-        # original csv.reader object is not iterable
-        # saving it's elements to this iterable gives greater flexibility
-        parser_iterable = []
-        row_container1 = []
-        row_container2 = []
-        half = int(self.__get_row_count())/2
-        for line in parser:
-            parser_iterable.append(line)
-            # slice the parser_iterable after it's first item 
-            # to leave the header row out of the result
-        file.close()
-        for item in parser_iterable[1:]:
-            row_container1.append(item)
-            if len(row_container1) > half:
-                break
-            row_container2.append(item)
-        master_container = {
-            'First_Half': row_container1, 
-            'Second_Half':row_container2
-            }
-        return master_container
+        encoding = self.__get_encoding()
+        with open(self.source, 'r', encoding=str(encoding)) as file:
+            parser = reader(file, delimiter=self.get_dialect().delimiter)
+            # original csv.reader object is not iterable
+            # saving it's elements to this iterable gives greater flexibility
+            parser_iterable = []
+            row_container1 = []
+            row_container2 = []
+            half = int(self.__get_row_count())/2
+            for line in parser:
+                parser_iterable.append(line)
+                # slice the parser_iterable after it's first item 
+                # to leave the header row out of the result
+            # file.close()
+            for item in parser_iterable[1:]:
+                row_container1.append(item)
+                if len(row_container1) > half:
+                    break
+                row_container2.append(item)
+            master_container = {
+                'First_Half': row_container1, 
+                'Second_Half':row_container2
+                }
+            return master_container
 
     def divide(self, number_of_parts:int) -> list:
         """
